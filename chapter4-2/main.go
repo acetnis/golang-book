e78b881c106c3df6fc4226bdb2327c9208ac9245
acetnis <ace_cu@hotmail.com>
2018-02-08 10:48:52 +0700

    Question #5

--- chapter4-2/main.go
+++ chapter4-2/main.go
@@ -8,4 +8,10 @@ func main() {
 	fmt.Scanf("%f", &input)
 	output := input * 2
 	fmt.Println(output)
+
+	fmt.Print("Enter Degree Fahrenheit: ")
+	var fdegree float64
+	fmt.Scanf("%f", &fdegree)
+	cdegree := (fdegree - 32) * 5 / 9
+	fmt.Printf("Degree Celcius is %.2f", cdegree)
 }
