e95cdd6172e0bbf31f6dd5be1d1342f12013d4c5
acetnis <ace_cu@hotmail.com>
2018-02-28 10:08:44 +0700

    Create engine.go for vendingMachine package

--- Exercise/vending-machine.go
+++ Exercise/vending-machine.go
@@ -1,72 +1,15 @@
 package main
+
 import "fmt"
+import "vendingMachine"
 
-type VendingMachine struct {
-	insertedMoney int
-	coins		  map[string]int
-	items		  map[string]int
-}
-
-func (m VendingMachine) InsertedMoney() int {
-	return m.insertedMoney
-}
-
-func (m *VendingMachine) InsertCoin(coin string) {
-	m.insertedMoney += m.coins[coin]
-
-}
-
-func (m *VendingMachine) ClearCoin() {
-	m.insertedMoney = 0
-}
-
-func (m VendingMachine) change(c int) string {
-	var str string
-	values := [...]int{10, 5, 2, 1}
-	coins := [...]string{"T", "F", "TW", "O"}
-
-	for i := 0; i < len(values); i++ {
-		if c >= values[i] {
-			str += ", " + coins[i]
-			c -= values[i]
-			i--
-		}
-	}
-	
-	return str
-}
-
-func (vm *VendingMachine) CoinReturn() string {
-	coins := vm.change(vm.insertedMoney)
-	vm.insertedMoney = 0
-	return coins[2:len(coins)]
-}
-
-func (m *VendingMachine) SelectSD() string {
-
-	price := m.items["SD"]
-	change := m.insertedMoney - price
-	return "SD" + m.change(change)
-}
-
-func (m *VendingMachine) SelectCC() string {
-	
-	price := m.items["CC"]
-	change := m.insertedMoney - price
-	return "CC" + m.change(change)
-}
-
-func (m *VendingMachine) SelectDW() string {
-	
-	price := m.items["DW"]
-	change := m.insertedMoney - price
-	return "DW" + m.change(change)
-}
+var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
+var items = map[string]int{"SD": 18, "CC": 12, "DW": 7}
 
 func main() {
-	var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
-	var items = map[string]int{"SD": 18, "CC": 12, "DW": 7}
-	vm := VendingMachine{coins: coins, items: items}
+
+	//vm := VendingMachine{coins: coins, items: items}
+	vm := vendingMachine.NewVendingMachine(coins, items)
 	fmt.Println("inserted Money:", vm.InsertedMoney())
 	// Inserted Money: 0
 	vm.InsertCoin("T")
